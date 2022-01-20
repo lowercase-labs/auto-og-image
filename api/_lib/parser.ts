2ee9ab5057fd0463680e44a4635044c6104768ca
Ayush <ayushkgupta1998@gmail.com>
2022-01-20 12:31:09 +0530

    linting

--- api/_lib/parser.ts
+++ api/_lib/parser.ts
@@ -6,36 +6,30 @@ import desc from 'metascraper-description';
 import title from 'metascraper-title';
 import { getPage } from './chromium';
 import { db } from '../plugins/firebase';
-import { doc, getDoc } from "firebase/firestore";
+import { doc, getDoc } from 'firebase/firestore';
 
 export async function parseURLRequest(req: Request, startSkipWord: string = '') {
-
-	
-
 	console.log('HTTP ' + req.url, startSkipWord);
 	const { query } = parse(req.url || '/', true);
 
-
-	const { url  } = query || {};
+	const { url } = query || {};
 	const scraper = metascraper([title(), desc()]);
 	const targetUrl = String(url) || '';
 
-	const domainRegex = /^(?:https?:\/\/)?(?:[^@\/\n]+@)?(?:www\.)?([^:\/?\n]+)/igm; //Regex found here - https://regex101.com/r/wN6cZ7/365
-	const regexArray =  domainRegex.exec(String(url));
+	const domainRegex = /^(?:https?:\/\/)?(?:[^@\/\n]+@)?(?:www\.)?([^:\/?\n]+)/gim; //Regex found here - https://regex101.com/r/wN6cZ7/365
+	const regexArray = domainRegex.exec(String(url));
 	console.log(regexArray);
 	const domain = regexArray ? regexArray[1] : '';
 
-	const docRef = doc(db, "enterprise-accounts", domain);
+	const docRef = doc(db, 'enterprise-accounts', domain);
 	const docSnap = await getDoc(docRef);
-	
+
 	if (docSnap.exists()) {
-		console.log("Document data:", docSnap.data());
+		console.log('Document data:', docSnap.data());
 	} else {
-	    // doc.data() will be undefined in this case
-		console.log("No such document!");
+		// doc.data() will be undefined in this case
+		console.log('No such document!');
 	}
-
-
 
 	const page = await getPage();
 	await page.goto(targetUrl, { waitUntil: 'networkidle2' });
@@ -44,7 +38,9 @@ export async function parseURLRequest(req: Request, startSkipWord: string = '') 
 	const metadata = await scraper({ html, url: targetUrl });
 	console.log(metadata);
 
-	let extension = '', imgTitle = '', imgDesc = '';
+	let extension = '',
+		imgTitle = '',
+		imgDesc = '';
 	if (metadata) {
 		imgTitle = metadata.title ? metadata.title : '';
 		imgDesc = metadata.description ? metadata.description : '';
